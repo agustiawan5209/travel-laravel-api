569e9ee69cbbca4f6c2dba670c559b57b55e4ad3
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2025-01-26 20:22:40 +0800

    Fix foreign key constraint in bookings migration to match table name

--- database/migrations/2025_01_26_110955_create_bookings_table.php
+++ database/migrations/2025_01_26_110955_create_bookings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->foreignId('jadwal_id')->constrained('jadwal_travels')->onDelete('cascade');
+            $table->foreignId('jadwal_id')->constrained('jadwal_travel')->onDelete('cascade');
             $table->date('tanggal');
             $table->enum('status', ['DONE','PENDING','CANCELED'])->comment('Status selesai ketika dibayar, menunggu, dibatalkan')->default('PENDING');
             $table->timestamps();
